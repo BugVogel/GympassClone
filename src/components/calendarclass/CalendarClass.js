86fc877b046edc9884f830fa9777e1fe291d3616
BugVogel <bugvogel@gmail.com>
2023-05-05 14:56:43 -0300

    added caroulsel of date

--- src/components/calendarclass/CalendarClass.js
+++ src/components/calendarclass/CalendarClass.js
@@ -1,8 +1,50 @@
 import React from 'react';
 import SectionTitle from '../sectiontitle/SectionTitle';
+import {CalendarClassContainer, DatesView, SubTitleView} from './styles';
+import {TextDefault} from '../../styles/UIComponents';
+import DateShow from './DateShow';
 
 const CalendarClass = () => {
-  return <SectionTitle title="CALENDÁRIO DE AULAS" />;
+  const date = new Date();
+  // console.log(date.getDate());
+  // const dd = String(today.getDate()).padStart(2, '0');
+  // const mm = String(today.getMonth() + 1).padStart(2, '0');
+
+  // const dayName = today
+  //   .toLocaleDateString('pt-br', {weekday: 'short'})
+  //   .split('.')[0];
+  // const monthName = today
+  //   .toLocaleDateString('pt-br', {month: 'short'})
+  //   .split('.')[0];
+  // const dateString = dayName + '-' + dd + '-' + monthName;
+
+  return (
+    <CalendarClassContainer>
+      <SectionTitle title="CALENDÁRIO DE AULAS" />
+      <SubTitleView>
+        <TextDefault fontSize={13}>
+          Selecione uma data para ver as aulas disponíveis ou agendada
+        </TextDefault>
+      </SubTitleView>
+      <DatesView horizontal showsHorizontalScrollIndicator={false}>
+        {[...Array(14)].map((_, index) => {
+          date.setDate(date.getDate() + 1);
+
+          const dd = String(date.getDate()).padStart(2, '0');
+          const dayName = date
+            .toLocaleDateString('pt-br', {weekday: 'short'})
+            .split('.')[0];
+          const monthName = date
+            .toLocaleDateString('pt-br', {month: 'short'})
+            .split('.')[0];
+
+          const dateString = dayName + '-' + dd + '-' + monthName;
+          // console.log(dateString);
+          return <DateShow key={index} date={dateString} />;
+        })}
+      </DatesView>
+    </CalendarClassContainer>
+  );
 };
 
 export default CalendarClass;
---
+++ src/components/calendarclass/styles.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const CalendarClassContainer = styled.View`
+  flex: 1;
+  background-color: white;
+`;
+
+export const SubTitleView = styled.View`
+  flex: 1;
+  flex-direction: row;
+  padding: 20px;
+  padding-left: 15px;
+`;
+
+export const DateShowContainer = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  padding-right: 30px;
+  padding-bottom: 20px;
+`;
+
+export const DatesView = styled.ScrollView`
+  flex: 1;
+  margin-left: 15px;
+`;
